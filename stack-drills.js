5ba3952c6cb3b1c0f86f62b5ad9a57574af89592
Albert Sare <thesundaymailman@gmail.com>
2018-07-27 13:19:42 -0700

    finished matchAnyParan

--- stack-drills.js
+++ stack-drills.js
@@ -90,9 +90,48 @@ function matchParan(str) {
   }
   return 'String is OK';
 }
+// console.log(matchParan('abc(d)e')); // expect 'string ok'
+// console.log(matchParan('abc(de')); // expect 'error: string open'
+// console.log(matchParan('abc)de')); // expect 'error: string closed at location column x'
+// console.log(matchParan('abc(((d)e')); // expect 'error: string open'
+// console.log(matchParan('abc(((d))))))e')); // expect 'error: string closed at column x'
 
-console.log(matchParan('abc(d)e')); // expect 'string ok'
-console.log(matchParan('abc(de')); // expect 'error: string open'
-console.log(matchParan('abc)de')); // expect 'error: string closed at location column x'
-console.log(matchParan('abc(((d)e')); // expect 'error: string open'
-console.log(matchParan('abc(((d))))))e')); // expect 'error: string closed at column x'
+function matchAnyParan(str) {
+  const tempStack = new Stack();
+  let expected;
+  // parse str
+  for (let i = 0; i < str.length; i ++) {
+    // push any '(', '{', or '[' into correct stack
+    if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+      tempStack.push(str[i]);
+      if (str[i] === '(') {
+        expected = ')';
+      } else if (str[i] === '{') {
+        expected = '}';
+      } else if (str[i] === '[') {
+        expected = ']';
+      }
+      continue;
+    }
+    // stack should NOT be popped unless a matched closing paran is parsed
+    if (str[i] === ')' && tempStack.top.value !== '(') {
+      return `Error: Expected ${expected} at column ${i} but detected ${str[i]} instead`;
+    } else if (str[i] === '}' && tempStack.top.value !== '{') {
+      return `Error: Expected ${expected} at column ${i} but detected ${str[i]} instead`;
+    } else if (str[i] === ']' && tempStack.top.value !== '[') {
+      return `Error: Expected ${expected} at column ${i} but detected ${str[i]} instead`;
+    } else {
+      tempStack.pop();
+    }
+    // or write it as: pop, unless its a bad case
+  }
+  // by end of parsing, stack should be empty if str property opened/closed
+  // but if string is open, stack will have something in it, so return 'error, string opened
+  if (tempStack.top) {
+    return 'Error: string is open';
+  }
+  return 'String is OK';
+}
+console.log(matchAnyParan('(){}[]')); // expect ok
+console.log(matchAnyParan('({)}')); // expect error at location 3, stating } is expected but got ) instead
+console.log(matchAnyParan('(((())]')); // expect error at location 3, stating } is expected but got ) instead
